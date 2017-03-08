6edcb8d3cb64950ce91ae4b9c0b587253ba154b8
jacebedo <acebedo.jp@hotmail.com>
2017-03-07 18:39:22 -0800

    Added Constructor functions for database and tables

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Databases/UserDatabaseHelper.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Databases/UserDatabaseHelper.java
@@ -10,8 +10,30 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class UserDatabaseHelper extends SQLiteOpenHelper {
 
+    // DATABASE META INFORMATION
     private static final String DATABASE_NAME="user.db";
     private static final int DATABASE_VERSION = 1;
+
+    // TABLE META INFORMATION
+    private static final String TABLE_NAME = "User-Information";
+    private static final String TABLE_ID = "_id";
+    private static final String TABLE_USERNAME = "Username";
+    private static final String TABLE_FIRSTNAME = "First Name";
+    private static final String TABLE_TOTALXP = "Total XP";
+    private static final String TABLE_LEARNINGXP = "Learning XP";
+    private static final String TABLE_PRACTICEXP = "Practice XP";
+
+    private static final String STATEMENT = "CREATE TABLE "  + TABLE_NAME + "( " +
+            TABLE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            TABLE_USERNAME + " TEXT NOT NULL, " +
+            TABLE_FIRSTNAME + " TEXT NOT NULL, " +
+            TABLE_TOTALXP + " INTEGER NOT NULL, " +
+            TABLE_LEARNINGXP + " INTEGER NOT NULL, " +
+            TABLE_PRACTICEXP + " INTEGER NOT NULL " +
+            ")";
+
+
+
 
     public UserDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -19,11 +41,13 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(null/*insert statmeent here*/);
+        db.execSQL(STATEMENT);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
