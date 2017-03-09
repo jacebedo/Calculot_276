27293e431d78158a29282ea57dad4e5423f175b1
jacebedo <acebedo.jp@hotmail.com>
2017-03-09 14:50:30 -0800

    Added a "isValidUser( )" method to UserDatabaseHelper.java

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Databases/UserDatabaseHelper.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Databases/UserDatabaseHelper.java
@@ -2,6 +2,7 @@ package calculotprototype.g14.cmpt276.calculot_prototype.Databases;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -14,7 +15,7 @@ import calculotprototype.g14.cmpt276.calculot_prototype.Classes.User;
 public class UserDatabaseHelper extends SQLiteOpenHelper {
 
     // DATABASE META INFORMATION
-    private static final String DATABASE_NAME="user.db";
+    private static final String DATABASE_NAME = "user.db";
     private static final int DATABASE_VERSION = 1;
 
     // TABLE META INFORMATION
@@ -28,7 +29,7 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_PRACTICEXP = "Practice XP";
 
     // onCreate statement
-    private static final String STATEMENT = "CREATE TABLE "  + TABLE_NAME + "( " +
+    private static final String STATEMENT = "CREATE TABLE " + TABLE_NAME + "( " +
             TABLE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             TABLE_USERNAME + " TEXT NOT NULL, " +
             TABLE_FIRSTNAME + " TEXT NOT NULL, " +
@@ -37,8 +38,6 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
             TABLE_LEARNINGXP + " INTEGER NOT NULL, " +
             TABLE_PRACTICEXP + " INTEGER NOT NULL " +
             ")";
-
-
 
 
     public UserDatabaseHelper(Context context) {
@@ -57,19 +56,47 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+
+    // Inserts a user into the database
     public long insertUser(User user) {
 
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues cv = new ContentValues();
-        cv.put(TABLE_USERNAME,user.getUsername());
-        cv.put(TABLE_FIRSTNAME,user.getFirstname());
-        cv.put(TABLE_PASSWORD,user.getPassword());
-        cv.put(TABLE_TOTALXP,0);
-        cv.put(TABLE_LEARNINGXP,0);
-        cv.put(TABLE_PRACTICEXP,0);
+        cv.put(TABLE_USERNAME, user.getUsername());
+        cv.put(TABLE_FIRSTNAME, user.getFirstname());
+        cv.put(TABLE_PASSWORD, user.getPassword());
+        cv.put(TABLE_TOTALXP, 0);
+        cv.put(TABLE_LEARNINGXP, 0);
+        cv.put(TABLE_PRACTICEXP, 0);
 
-        return db.insert(TABLE_NAME,null,cv);
+        return db.insert(TABLE_NAME, null, cv);
+
+    }
+
+    // Looks for the user in the database. If found and username & password are equivalent, return true. Else, return false
+    public boolean isValidUser(String _username, String _password) {
+
+        // Initialize Database and Cursor
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT " + TABLE_USERNAME + ", " + TABLE_PASSWORD + " FROM " + TABLE_NAME, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                String username = cursor.getString(0);
+                String password = cursor.getString(2);
+
+                if ( username == _username && password == _password) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            } while (cursor.moveToNext());
+        }
+        else{
+            return false;
+        }
 
     }
 
