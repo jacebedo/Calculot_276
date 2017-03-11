b0d03f67fe009a0e3e795a5f69b873b5a0253b3f
repeters <repeters@sfu.ca>
2017-03-10 16:46:05 -0800

    Created AddUser.java (unfinished) Created add_user.xml Created UI for adding new user form (unfinished)

---
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/AddUser.java
@@ -0,0 +1,15 @@
+package calculotprototype.g14.cmpt276.calculot_prototype;
+
+import android.os.Bundle;
+
+/**
+ * Created by Ryan on 3/10/2017.
+ */
+
+public class AddUser extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+    }
+}
