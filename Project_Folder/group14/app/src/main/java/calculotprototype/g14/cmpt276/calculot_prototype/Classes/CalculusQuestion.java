fbbf366b77e0fee54c1a7de3befc8db8f044fd86
kza21 <kza21@sfu.ca>
2017-03-10 23:24:38 -0800

    Added CalculusQuestionGenerator.java + CalculusQuestion.java basic form to get started

---
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Classes/CalculusQuestion.java
@@ -0,0 +1,13 @@
+package calculotprototype.g14.cmpt276.calculot_prototype.Classes;
+
+public class CalculusQuestion {
+    //implement
+    int NumberOfTerms;
+    //int[][] ExpressionArray = new int[][2]; 2d array for [terms][coefficient/powers]?
+
+    //constructor
+    public CalculusQuestion(int _numberofterms) {
+        //implement
+        NumberOfTerms = _numberofterms;
+    }
+}
