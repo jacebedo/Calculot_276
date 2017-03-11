6845e0408f35db122db84d54fe7cd9f2c3475154
repeters <repeters@sfu.ca>
2017-03-10 17:02:59 -0800

    Got references to edittexts in add user

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/AddUser.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/AddUser.java
@@ -2,6 +2,7 @@ package calculotprototype.g14.cmpt276.calculot_prototype;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 /**
@@ -13,6 +14,28 @@ public class AddUser extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_user);
+    }
+
+    public void constRun(){
+        EditText firstNameET = (EditText) findViewById(R.id.addUser_firstName);
+        EditText usernameET = (EditText) findViewById(R.id.addUser_username);
+        EditText passwordET = (EditText) findViewById(R.id.addUser_password);
+        EditText password2ET = (EditText) findViewById(R.id.addUser_retypepassword);
+
+        String passwordSt = passwordET.getText().toString();
+        String password2St = password2ET.getText().toString();
+
+        if (passwordSt.equals(password2St)){
+            Toast.makeText(this, "They're the same!", Toast.LENGTH_SHORT).show();
+        }
+        //3/10/17 - Not working, maybe put it after button press?
+
+        /*button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Do stuff
+            }
+        });*/
     }
 
     public void add_user_onClick_signUp(View view){
