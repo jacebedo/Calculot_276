2dc4d876915f63b7b70e01a4fbf468c66537615f
repeters <repeters@sfu.ca>
2017-03-10 16:56:00 -0800

    Tested Sign up form (non-functional) Created fade_in_fast.xml for buttons

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/AddUser.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/AddUser.java
@@ -1,6 +1,8 @@
 package calculotprototype.g14.cmpt276.calculot_prototype;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Ryan on 3/10/2017.
@@ -10,6 +12,10 @@ public class AddUser extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
+        setContentView(R.layout.add_user);
+    }
+
+    public void add_user_onClick_signUp(View view){
+        Toast.makeText(this, "Clicked Sign Up... woo!", Toast.LENGTH_SHORT).show();
     }
 }
--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/MainMenu.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/MainMenu.java
@@ -1,6 +1,8 @@
 package calculotprototype.g14.cmpt276.calculot_prototype;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -17,8 +19,13 @@ public class MainMenu extends MainActivity{
 
         Button sign_up_button = (Button) findViewById(R.id.sign_up_button); //Get reference to sign up button
         Button login_button = (Button) findViewById(R.id.login_button); //Get reference to login button
-        Animation fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in); //Get reference to fade_in animation
+        Animation fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in_fast); //Get reference to fade_in animation
         sign_up_button.startAnimation(fadein);
         login_button.startAnimation(fadein);
+    }
+
+    public void main_onClick_signUp(View view){
+        Intent goToSignUpForm = new Intent(MainMenu.this, AddUser.class);
+        startActivity(goToSignUpForm);
     }
 }
