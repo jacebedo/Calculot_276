405a89e6aa3e0bcb8b85dc4949662ccb804f4d55
Ryan Peterson <repeters@sfu.ca>
2017-03-12 17:01:01 -0700

    created login activity

---
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Login.java
@@ -0,0 +1,15 @@
+package calculotprototype.g14.cmpt276.calculot_prototype;
+
+import android.os.Bundle;
+
+/**
+ * Created by Ryan on 3/12/2017.
+ */
+
+public class Login extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
