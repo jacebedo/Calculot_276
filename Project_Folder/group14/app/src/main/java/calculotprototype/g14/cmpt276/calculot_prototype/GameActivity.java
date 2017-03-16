870807ee72d00922bd1aff1aa55ca56006a9e78e
jacebedo <acebedo.jp@hotmail.com>
2017-03-16 09:30:49 -0700

    Added countdown timer functionality for GameActivity.class & prevented GameOverActivity from going back into GameActivity after pressing the android back button.

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/GameActivity.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/GameActivity.java
@@ -1,6 +1,7 @@
 package calculotprototype.g14.cmpt276.calculot_prototype;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,13 +18,15 @@ public class GameActivity extends AppCompatActivity {
 
         // Health, XPGained, Level, Completed
         final int[] info = {5,0,1,0};
-        final CalcQuestion calc = new CalcQuestion(0,1);
+        int topic = 0;
+        final CalcQuestion calc = new CalcQuestion(topic,1);
 
 
+        // Set Initial TextViews
         final TextView healthfield = (TextView) findViewById(R.id.game_health);
-        healthfield.setText("Health: " + Integer.toString(info[0]));
-
         final TextView xpfield = (TextView) findViewById(R.id.game_xpgained);
+
+        healthfield.setText("Health: " + Integer.toString(info[0]));
         xpfield.setText("XP Gained: " + Integer.toString(info[1]));
 
         // Wire up all of the TextViews
@@ -33,7 +36,26 @@ public class GameActivity extends AppCompatActivity {
         final TextView answer3 = (TextView) findViewById(R.id.game_answer3);
         final TextView answer4 = (TextView) findViewById(R.id.game_answer4);
         final Intent gameOver = new Intent(GameActivity.this, GameOverActivity.class);
-        setQuestion(question, answer1, answer2, answer3, answer4, calc);
+
+        // Set up countdown timer ( 5 seconds currently - 1s = 1000ms)
+        final CountDownTimer timer = new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+            @Override
+            public void onFinish() {
+                info[0]--;
+                setQuestion(question, answer1, answer2, answer3, answer4, calc, this);
+                healthfield.setText("Health: " + Integer.toString(info[0]));
+                if (info[0] == 0) {
+                    startActivity(gameOver);
+                }
+            }
+        };
+
+       // Set the first answer field
+        setQuestion(question, answer1, answer2, answer3, answer4, calc, timer);
 
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +66,7 @@ public class GameActivity extends AppCompatActivity {
                     checkLevel(info);
 
                     calc.getNewQuestion(0, info[2]);
-                    setQuestion(question, answer1, answer2, answer3, answer4, calc);
+                    setQuestion(question, answer1, answer2, answer3, answer4, calc,timer);
                 } else {
                     // Do something else if incorrect
                     info[0]--;
@@ -57,7 +79,7 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
-
+        // Set up the second answer field
         answer2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,7 +89,7 @@ public class GameActivity extends AppCompatActivity {
                     checkLevel(info);
 
                     calc.getNewQuestion(0, info[2]);
-                    setQuestion(question, answer1, answer2, answer3, answer4, calc);
+                    setQuestion(question, answer1, answer2, answer3, answer4, calc,timer);
                 } else {
                     // Do something else if wrong
                     info[0]--;
@@ -79,6 +101,8 @@ public class GameActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Set up the third answer field
         answer3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,7 +112,7 @@ public class GameActivity extends AppCompatActivity {
                     checkLevel(info);
 
                     calc.getNewQuestion(0, info[2]);
-                    setQuestion(question, answer1, answer2, answer3, answer4, calc);
+                    setQuestion(question, answer1, answer2, answer3, answer4, calc,timer);
                 } else {
                     // Do something else if wrong
                     info[0]--;
@@ -100,6 +124,8 @@ public class GameActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Set up the fourth answer field
         answer4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,7 +135,7 @@ public class GameActivity extends AppCompatActivity {
                     checkLevel(info);
 
                     calc.getNewQuestion(0, info[2]);
-                    setQuestion(question, answer1, answer2, answer3, answer4, calc);
+                    setQuestion(question, answer1, answer2, answer3, answer4, calc,timer);
                 } else {
                     // Do something else if wrong
                     info[0]--;
@@ -125,13 +151,16 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
+    // Checks if the user can advance into the next level
     private boolean checkLevel(int[] info) {
 
+        // End the game ( Level 3 and the user has answered 10 questions at level 3)
         if (info[2] == 3 && info[3] % 10 == 0) {
             info[1] += 1000;
             return true;
         }
 
+        // If the user has answered 10 questions at the current level, give the user XP and advance into the next level
         if (info[3] % 10 == 0) {
             info[1]+= 100;
             info[2]++;
@@ -145,14 +174,17 @@ public class GameActivity extends AppCompatActivity {
     }
 
 
+    // Sets up the randomly generated question and start the timer up;
     public void setQuestion(TextView question, TextView answer1, TextView answer2,
-                            TextView answer3, TextView answer4, CalcQuestion c) {
+                            TextView answer3, TextView answer4, CalcQuestion c, CountDownTimer timer) {
 
         question.setText(c.getQuestion());
         answer1.setText(c.getAnswer_1());
         answer2.setText(c.getAnswer_2());
         answer3.setText(c.getAnswer_3());
         answer4.setText(c.getAnswer_4());
+
+        timer.start();
 
     }
 }
--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/GameOverActivity.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/GameOverActivity.java
@@ -1,5 +1,6 @@
 package calculotprototype.g14.cmpt276.calculot_prototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class GameOverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        // temporarily goes into mainmenu. This will change.
+        Intent goToMainMenu = new Intent(GameOverActivity.this,MainMenu.class);
+        startActivity(goToMainMenu);
     }
 }
