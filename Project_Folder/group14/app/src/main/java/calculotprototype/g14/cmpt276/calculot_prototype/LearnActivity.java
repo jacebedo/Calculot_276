91cfcf675597b2d6ddaf4523984b6b0143e1ee50
lkylych <L24101996>
2017-03-15 21:41:36 -0700

    Now if you plug in right values color of word Differentiation changes to green

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/LearnActivity.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/LearnActivity.java
@@ -1,9 +1,14 @@
 package calculotprototype.g14.cmpt276.calculot_prototype;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -22,7 +27,7 @@ public class LearnActivity extends YouTubeBaseActivity {
         listener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("sL6MC-lKOrw");
+                youTubePlayer.loadVideo("rAof9Ld5sOg");
             }
 
             @Override
@@ -31,6 +36,21 @@ public class LearnActivity extends YouTubeBaseActivity {
             }
         };
         yView.initialize("AIzaSyDCRngE35azPRQyUeK5GLH9jSUFnhpPn7g", listener);
+        Button btn = (Button) findViewById(R.id.learnbtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText q11 = (EditText) findViewById(R.id.inputq11);
+                EditText q12 = (EditText) findViewById(R.id.inputq12);
+                EditText q21 = (EditText) findViewById(R.id.inputq21);
+                EditText q31 = (EditText) findViewById(R.id.inputq31);
+                TextView dif = (TextView) findViewById(R.id.diffR);
+                if(q11.getText().toString().equals("y") && q12.getText().toString().equals("x"))
+                    if(q21.getText().toString().equals("limit"))
+                        if(q31.getText().toString().equals("A"))
+                            dif.setTextColor(Color.GREEN);
+            }
+        });
 
     }
 }
