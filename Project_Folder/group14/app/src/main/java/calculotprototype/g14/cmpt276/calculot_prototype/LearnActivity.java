74500b6c67f36a031ad7038288eea740dc763643
lkylych <L24101996>
2017-03-16 20:22:31 -0700

    If answers are right next click on the button will send to the profile activity

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/LearnActivity.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/LearnActivity.java
@@ -1,5 +1,6 @@
 package calculotprototype.g14.cmpt276.calculot_prototype;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -40,15 +41,28 @@ public class LearnActivity extends YouTubeBaseActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Button btn = (Button) findViewById(R.id.learnbtn);
                 EditText q11 = (EditText) findViewById(R.id.inputq11);
                 EditText q12 = (EditText) findViewById(R.id.inputq12);
                 EditText q21 = (EditText) findViewById(R.id.inputq21);
                 EditText q31 = (EditText) findViewById(R.id.inputq31);
+                EditText q41 = (EditText) findViewById(R.id.inputq41);
+                EditText q51 = (EditText) findViewById(R.id.inputq51);
                 TextView dif = (TextView) findViewById(R.id.diffR);
-                if(q11.getText().toString().equals("y") && q12.getText().toString().equals("x"))
-                    if(q21.getText().toString().equals("limit"))
-                        if(q31.getText().toString().equals("A"))
-                            dif.setTextColor(Color.GREEN);
+                if(dif.getCurrentTextColor() != Color.GREEN) {
+                    if (q11.getText().toString().equals("y") && q12.getText().toString().equals("x"))
+                        if (q21.getText().toString().equals("limit"))
+                            if (q31.getText().toString().equals("A"))
+                                if (q41.getText().toString().equals("f(x)+g(x)"))
+                                    if (q51.getText().toString().equals("lna")) {
+                                        dif.setTextColor(Color.GREEN);
+                                        btn.setText("Back to profile");
+                                    }
+                }
+                else{
+                    Intent profile = new Intent(LearnActivity.this, Profile.class);
+                    startActivity(profile);
+                }
             }
         });
 
