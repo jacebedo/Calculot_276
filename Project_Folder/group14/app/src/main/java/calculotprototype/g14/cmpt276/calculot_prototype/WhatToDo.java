78826576a18bd1d75454d3dd11aa44793d2ddbb8
repeters <repeters@sfu.ca>
2017-03-17 20:59:21 -0700

    WhatToDo starts after log in, and links to Game, Learn, or Profile

---
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/WhatToDo.java
@@ -0,0 +1,34 @@
+package calculotprototype.g14.cmpt276.calculot_prototype;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import calculotprototype.g14.cmpt276.calculot_prototype.calcGame.GameActivity;
+
+/**
+ * Created by Ryan on 3/17/2017.
+ */
+
+public class WhatToDo extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.what_to_do);
+    }
+
+    public void what_to_do_onClick_profile(View view){
+        Intent goToProfile = new Intent(WhatToDo.this, Profile.class);
+        startActivity(goToProfile);
+    }
+
+    public void what_to_do_onClick_learn(View view){
+        Intent goToLearn = new Intent(WhatToDo.this, LearnActivity.class);
+        startActivity(goToLearn);
+    }
+
+    public void what_to_do_onClick_game(View view){
+        Intent goToGameActivity = new Intent(WhatToDo.this, GameActivity.class);
+        startActivity(goToGameActivity);
+    }
+}
