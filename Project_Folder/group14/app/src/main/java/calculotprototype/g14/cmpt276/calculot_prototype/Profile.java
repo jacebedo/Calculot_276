b532bda569c2d595104940712e701d87d868a142
repeters <repeters@sfu.ca>
2017-04-04 02:13:53 -0700

    Achievements working properly :)

--- Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Profile.java
+++ Project_Folder/group14/app/src/main/java/calculotprototype/g14/cmpt276/calculot_prototype/Profile.java
@@ -19,7 +19,7 @@ import calculotprototype.g14.cmpt276.calculot_prototype.calcGame.GameActivity;
 
 public class Profile extends MainActivity {
     User user; //Global User to be used by functions
-    int MAX_PRACTICE_XP = 100;
+    int MAX_PRACTICE_XP = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +30,13 @@ public class Profile extends MainActivity {
 
         UserDatabaseHelper DB = new UserDatabaseHelper(this);
         user = DB.getUser(username); //Gets User object from database (includes all user info)
+        DB.addPracticeXP(username, 120);
 
         TextView usernameTV = (TextView) findViewById(R.id.profile_username);
         TextView firstNameTV = (TextView) findViewById(R.id.profile_firstName);
         usernameTV.setText(user.getUsername());
         firstNameTV.setText(user.getFirstname());
+
     }
 
     @Override
@@ -45,18 +47,15 @@ public class Profile extends MainActivity {
 
     public void updateUI(){
         //Get reference to textviews
-        TextView learningXPTV = (TextView) findViewById(R.id.profile_learningXP_text);
         TextView practiceXPTV = (TextView) findViewById(R.id.profile_practiceXP_text);
         TextView levelTV = (TextView) findViewById(R.id.profile_level);
         //Get strings for textview and cast as string (because it's messy)
-        String learningXPSt = getApplicationContext().getString(R.string.learning_xp);
         String practiceXPSt = getApplicationContext().getString(R.string.practice_xp);
         String levelSt = getApplicationContext().getString(R.string.level);
 
-        learningXPTV.setText(learningXPSt); //TODO: make learningXP track number of modules finished instead. Each module adds y XP to total
-
+        int XPneededfornextlevel = MAX_PRACTICE_XP - user.getRemainderXP();
         //This is messy too
-        String practiceXPoutOfSt = Integer.toString(user.getPracticeXP()) + "/" + Integer.toString(MAX_PRACTICE_XP);
+        String practiceXPoutOfSt = Integer.toString(user.getRemainderXP()) + "/" + Integer.toString(MAX_PRACTICE_XP) + "\n" + Integer.toString(XPneededfornextlevel) + " XP needed for next level.";
         //Sets TextView so user can see int of how much XP they have
         practiceXPTV.setText(practiceXPSt + " " + practiceXPoutOfSt);
         //Sets level TextView to the players level
@@ -73,5 +72,24 @@ public class Profile extends MainActivity {
         else if (avatarUse == 2) avatarPicture.setImageResource(R.drawable.malewizard2);
         else if (avatarUse == 3) avatarPicture.setImageResource(R.drawable.malewizard3);
         //Else leave at default
+
+        //Get reference to achievement images and lock status texts
+        ImageView achievement1img = (ImageView) findViewById(R.id.profile_achievement_image1);
+        ImageView achievement2img = (ImageView) findViewById(R.id.profile_achievement_image2);
+        ImageView achievement3img = (ImageView) findViewById(R.id.profile_achievement_image3);
+        TextView achievement1text = (TextView) findViewById(R.id.profile_achievement1_lockstatus);
+        TextView achievement2text = (TextView) findViewById(R.id.profile_achievement2_lockstatus);
+        TextView achievement3text = (TextView) findViewById(R.id.profile_achievement3_lockstatus);
+
+        //Update achievements if applicable
+        if (user.getlevel() >= 5){
+            achievement1img.setImageResource(R.drawable.bronze);
+            achievement1text.setText("Unlocked"); }
+        if (user.getlevel() >= 10){
+            achievement2img.setImageResource(R.drawable.silver);
+            achievement2text.setText("Unlocked"); }
+        if (user.getlevel() >= 15){
+            achievement3img.setImageResource(R.drawable.gold);
+            achievement3text.setText("Unlocked"); }
     }
 }
